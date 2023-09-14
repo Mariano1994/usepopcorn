006b906ae0e429ad285350d12a01f57ae0e5106a
Mariano <marianocapiliku@gmail.com>
2023-09-14 23:06:02 +0100

    .

---
+++ src/components/Main/WatchedMoviesListBox/WatchedMoviesList.jsx
@@ -0,0 +1,15 @@
+import { WatchedMovie } from "./WatchedMovie";
+
+
+export function WatchedMoviesList({watched}) {
+
+  return (
+    <>
+      <ul className="list">
+        {watched.map((movie) => (
+          <WatchedMovie movie={movie} key={movie.imdbID}/>
+        ))}
+      </ul>
+    </>
+  );
+}
