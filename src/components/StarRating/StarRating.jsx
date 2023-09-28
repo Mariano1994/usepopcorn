ba4ed5eae8bf029bd7427dcb0e5186b79deef6af
Mariano <marianocapiliku@gmail.com>
2023-09-28 10:14:49 +0100

    Costumize Star component, passing a array of messa

--- src/components/StarRating/StarRating.jsx
+++ src/components/StarRating/StarRating.jsx
@@ -11,14 +11,22 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeiht: "1",
-  margin: "0",
-};
-
-export function StarRating({ maxiRating = 5 }) {
+export function StarRating({
+  maxiRating = 5,
+  color = "#fcc419",
+  size = 24,
+  className = "",
+  messages = [],
+}) {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempoRating] = useState(0);
+
+  const textStyle = {
+    lineHeiht: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
 
   function handleRating(rating) {
     setRating(rating);
@@ -26,7 +34,7 @@ export function StarRating({ maxiRating = 5 }) {
 
   return (
     <>
-      <div style={containerStyle}>
+      <div style={containerStyle} className={className}>
         <div style={starContainerStyle}>
           {Array.from({ length: maxiRating }, (__, i) => {
             return (
@@ -36,11 +44,17 @@ export function StarRating({ maxiRating = 5 }) {
                 onRate={() => handleRating(i + 1)}
                 onHoverIn={() => setTempoRating(i + 1)}
                 onHoverOut={() => setTempoRating(0)}
+                color={color}
+                size={size}
               />
             );
           })}
         </div>
-        <p style={textStyle}>{tempRating || rating || ""}</p>
+        <p style={textStyle}>
+          {messages.length === maxiRating
+            ? messages[tempRating ? tempRating - 1 : rating - 1]
+            : tempRating || rating || ""}
+        </p>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { StarRating } from './components/StarRating/StarRating'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StarRating } from "./components/StarRating/StarRating";
 // import App from './App.jsx'
 // import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
 
-    
-    <StarRating maxiRating={8}/>
-  </React.StrictMode>,
-)
+    <StarRating
+      maxiRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={20} color="#121214" className="test" />
+  </React.StrictMode>
+);
