68af06a332f9e4c3ec906ba74475cc85dd5ff6e7
Mariano <marianocapiliku@gmail.com>
2023-10-24 17:20:21 +0100

    Adding loading component Show loading component while fetching

--- src/components/Main/MovieDetails/MovieDetails.jsx
+++ src/components/Main/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { Loading } from "../../Loading/Loading";
 import { StarRating } from "../../StarRating/StarRating";
 
 const KEY = "8c1320d";
 
 export function MovieDetails({ selectedId, onCloseMovieDetail }) {
   const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     Title: title,
@@ -22,47 +24,55 @@ export function MovieDetails({ selectedId, onCloseMovieDetail }) {
 
   useEffect(() => {
     async function getMovieDetails() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
+      setIsLoading(false);
     }
-
+    
     getMovieDetails();
   }, [selectedId]);
 
   return (
     <>
       <div className="details">
-        <header>
-          <button className="btn-back" onClick={onCloseMovieDetail}>
-            &larr;
-          </button>
+        {isLoading ? 
+          <Loading />
+         : 
+          <>
+            <header>
+              <button className="btn-back" onClick={onCloseMovieDetail}>
+                &larr;
+              </button>
 
-          <img src={poster} alt={`Poster of ${movie} movie`} />
-          <div className="details-overview">
-            <h2>{title}</h2>
-            <p>
-              {released} &bull; {runTime}
-            </p>
-            <p>{genre}</p>
-            <p>
-              <span>⭐️</span> {imdbRating} IMDb rating
-            </p>
-          </div>
-        </header>
+              <img src={poster} alt={`Poster of ${movie} movie`} />
+              <div className="details-overview">
+                <h2>{title}</h2>
+                <p>
+                  {released} &bull; {runTime}
+                </p>
+                <p>{genre}</p>
+                <p>
+                  <span>⭐️</span> {imdbRating} IMDb rating
+                </p>
+              </div>
+            </header>
 
-        <section>
-          <div className="rating">
-            <StarRating maxiRating={10} size={22} key={selectedId} />
-          </div>
-          <p>
-            <em>{plot}</em>
-          </p>
-          <p>Staring {actors}</p>
-          <p>Directed by {director}</p>
-        </section>
+            <section>
+              <div className="rating">
+                <StarRating maxiRating={10} size={22} key={selectedId} />
+              </div>
+              <p>
+                <em>{plot}</em>
+              </p>
+              <p>Staring {actors}</p>
+              <p>Directed by {director}</p>
+            </section>
+          </>
+        }
       </div>
     </>
   );
